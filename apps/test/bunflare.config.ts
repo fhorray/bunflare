655f973ccb706cfc00a1f680ba963b789bb598c0
Francy Santos <francy.horray@gmail.com>
2026-05-02 05:06:54 -0300

    :sparkles: improvements in init projects UI

--- apps/test/bunflare.config.ts
+++
@@ -1,9 +0,0 @@
-import { bunflare, type BunflareConfig } from "bunflare";
-
-export default {
-  entrypoint: "./src/index.ts",
-  frontend: {
-    entrypoint: "./public/index.html",
-    outdir: "./dist/public",
-  },
-} satisfies BunflareConfig;
